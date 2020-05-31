c30905aa4a8334fd70c4a154eca8b9ac60b7df01
rahulmangyani <rahulmangyani18@gmail.com>
2020-05-31 17:47:30 +0530

    git remote add origin https://github.com/Rahulm-ai/Todo.git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
-}
+  title = 'todo-app'
+  todoArray=[] ;
+  addTodo(value){    this.todoArray.push(value)  ;console.log(this.todo)    }
+  todo(todo: any) {
+    throw new Error("Method not implemented.");
+  }
+}   
+function deleteItem(todo)
+{   
+  for(let i=0 ;i<= this.todoArray.length ;i++)
+  {    if(todo== this.todoArray[i])
+    {     this.todoArray.splice(i,1)    }   
+  }
+  console.log("delete item");
+  }  
